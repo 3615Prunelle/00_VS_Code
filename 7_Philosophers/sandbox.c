95c138b66534ce3c3b1c6b117c2d3f18eba4c75e
3615Prunelle <elfrasquitofresquito@gmail.com>
2025-09-19 13:12:30 +0200

    Ready to carry on threads learning & testing from home

---
+++ 7_Philosophers/sandbox.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+# define NC "\e[0m"
+# define YELLOW "\e[1;33m"
+# define GREEN "\e[1;32m"
+# define BLUE "\e[1;34m"
+# define MAGENTA "\e[1;35m"
+# define RED "\e[1;31m"
+# define CYAN "\e[1;36m"
+
+int j = 0;
+
+void *sandbox_thread(void *data)
+{
+	pthread_t	which_ID_is_curently_here;
+	which_ID_is_curently_here = pthread_self();			// Pour savoir quel ID est rentré cette fois
+	// printf("%sj is [%d]\tID [%lu] - About to sleep for 2 secs\n%s", MAGENTA, j, which_ID_is_curently_here, NC);
+	// sleep(2);
+	// j++;
+	// printf("%sj is [%d]\tID [%lu] - Slept for 2 secs\n%s", MAGENTA, j, which_ID_is_curently_here, NC);
+	printf("%sID [%lu] found j at this value : [%d] - About to enter a loop\n%s", MAGENTA, which_ID_is_curently_here, j, NC);
+	// Process A peut entrer avant Process B, mais en sortir apres
+	for (size_t i = 0; i < 1000000; i++)
+	{
+		j++;
+	}
+
+	printf("%sID [%lu] left j at this value : [%d] - Out of the loop\n%s", MAGENTA, which_ID_is_curently_here, j, NC);
+	return (NULL);
+}
+
+int main(void)
+{
+//  -------- Test pour connaitre le temps écoulé entre 2 actions -------------------------------------------------------------------------- |
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	printf("Seconds: %ld, Microseconds: %ld\n", tv.tv_sec, tv.tv_usec);
+	sleep(1);
+	gettimeofday(&tv, NULL);
+	printf("Seconds: %ld, Microseconds: %ld\n", tv.tv_sec, tv.tv_usec);
+//  --------------------------------------------------------------------------------------------------------------------------------------- |
+// //  -------- Test pour connaitre l'ID du thread principal (celui qui execute main), et demonstration qu'on ne peut pas le join ------------ |
+// 	pthread_t main_thread = pthread_self();
+// 	printf("Main thread id = %lu\n", main_thread);
+// 	int ret = pthread_join(main_thread, NULL);
+// 	if (ret != 0)
+// 		printf("pthread_join failed with code %d\n", ret);
+// //  --------------------------------------------------------------------------------------------------------------------------------------- |
+
+	pthread_t	ID_1;
+	pthread_t	ID_2;
+
+	printf("%sFirst j is [%d]\n%s", YELLOW, j, NC);
+
+// A partir de la, l'ensemble forme un tout execute' randomly
+// Le 1er thread s'executera toujous en 1er
+	pthread_create(&ID_1, NULL, &sandbox_thread, NULL);		// Lui passer la fonction sandbox_thread ou un pointeur (&sandbox_thread) revient au meme
+	pthread_create(&ID_2, NULL, &sandbox_thread, NULL);
+	pthread_join(ID_1, NULL);
+	printf("%sj is [%d]\n%s", GREEN, j, NC);
+	printf("%sj is [%d]\n%s", BLUE, j, NC);
+	printf("%sID_1 is [%lu]\tID_2 is [%lu]\n%s", RED, ID_1, ID_2, NC);
+
+	printf("%sj is [%d]\n%s", CYAN, j, NC);
+	pthread_join(ID_2, NULL);
+// The 2 threads have been joined (= they finished their execution), nothing else can happen from here
+	printf("%sj is [%d]\n%s", YELLOW, j, NC);
+
+	return (0);
+}
